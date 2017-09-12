5db18ad302892943a83514e1e66ee5fcfc6062cb
U-DESKTOP-K1B269C\Keith <keithstellyes@github.com>
2017-09-12 09:14:02 -0700

    kw_globals documentation -- closes #38

---
+++ kw_globals.py
@@ -0,0 +1,9 @@
+'''
+
+May contain the following variables:
+
+SCRIPT_BASE_DIR --- The base directory of this project. Used for a consistent
+                    and absolute importing across files, but may be leveraged
+                    for other purposes.
+
+'''
